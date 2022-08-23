2aea9390c4be9d9d0a23c377e5dcb1f774689f43
buyanturb <buyantur1124@gmail.com>
2022-08-23 17:11:58 +0800

    sidebar nemsen

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Navbar />
       <BigBanner />
       <Winter />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopHeader from './TopHeader';
 
 import logo from '../../assets/img/logo.svg'
@@ -7,8 +7,18 @@ import like from '../../assets/img/like.svg'
 
 import bag from '../../assets/img/bag.svg'
 
+import search from '../../assets/img/search.svg'
+
+import hamburger from '../../assets/img/hamburger.svg'
+
 const Navbar = () =>
 {
+    const [isOpen, setIsopen] = useState(false);
+
+    const ToggleSidebar = () => {
+        isOpen === true ? setIsopen(false) : setIsopen(true);
+    }
+
     const menus = [
         {
             name: "women"
@@ -29,19 +39,51 @@ const Navbar = () =>
 
     return (
 		<div className="z-10 text-white">
+
+
+
+            <div className={`sidebar ${isOpen == true ? 'active' : ''}`}>
+                <div className="sd-header">
+                    <h4 className="mb-0">Sidebar Header</h4>
+                    <div className="btn btn-primary" onClick={ToggleSidebar}><i className="fa fa-times"></i></div>
+                </div>
+                <div className="sd-body">
+                    <ul>
+                        {
+                            menus.map(
+                                (el, idx) =>
+                                {
+                                    return(
+                                        <li key={idx}><a className="sd-link">{el.name}</a></li>
+                                    )
+                                }
+                            )
+                        }
+
+                    </ul>
+                </div>
+            </div>
+            <div className={`sidebar-overlay ${isOpen == true ? 'active' : ''}`} onClick={ToggleSidebar}></div>
+            
             <TopHeader />
-            <div className="h-[88px] px-2 sm:px-11 flex items-center">
+            <div className="h-[53px] md:h-[88px] px-2 sm:px-11 flex items-center">
                 <div className="w-1/3">
                     <ul className="hidden md:flex uppercase cursor-pointer menus">
-                        {menus.map(
-                            (el, idx) =>
-                            {
-                                return(
-                                    <li className="mr-10" key={idx}><a>{el.name}</a></li>
-                                )
-                            }
-                        )}
+                        {
+                            menus.map(
+                                (el, idx) =>
+                                {
+                                    return(
+                                        <li className="mr-10" key={idx}><a>{el.name}</a></li>
+                                    )
+                                }
+                            )
+                        }
                     </ul>
+                    <div className="flex md:hidden">
+                        <img className="cursor-pointer mr-5" src={hamburger} alt="hamburger" onClick={ToggleSidebar} />
+                        <img className="cursor-pointer" src={search} alt="search" />
+                    </div>
                 </div>
                 <div className="w-1/3 flex justify-center">
                     <img src={logo} alt="Logo" />
