3309602313f4e6fcdbae20f699d7ea0d705c4b61
buyanturb <buyantur1124@gmail.com>
2022-08-23 19:31:58 +0800

    responsive zassan

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -63,7 +63,7 @@ const Navbar = () =>
             <TopHeader />
             <div className="h-[53px] md:h-[88px] px-2 sm:px-11 flex items-center">
                 <div className="w-1/3">
-                    <ul className="hidden md:flex uppercase cursor-pointer menus">
+                    <ul className="hidden lg:flex flex uppercase cursor-pointer menus">
                         {
                             menus.map(
                                 (el, idx) =>
@@ -75,7 +75,7 @@ const Navbar = () =>
                             )
                         }
                     </ul>
-                    <div className="flex md:hidden">
+                    <div className="flex lg:hidden">
                         <img className="cursor-pointer mr-5" src={hamburger} alt="hamburger" onClick={ToggleSidebar} />
                         <div className="container relative mobile-search">
                             <input placeholder='Search...' className='js-search' type="text" />
@@ -87,7 +87,7 @@ const Navbar = () =>
                     <img src={logo} alt="Logo" />
                 </div>
                 <div className="flex items-center w-1/3 justify-end">
-                    <input className="w-[260px] sm:block hidden placeholder:text-black block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="search" style={{ width: '260px' }} />
+                    <input className="w-[260px] lg:block hidden placeholder:text-black bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="search" style={{ width: '260px' }} />
                     <img className="px-5 cursor-pointer" src={like} alt="like" />
                     <img className="cursor-pointer" src={bag} alt="bag" />
                 </div>
