afc4cc6934945f03c55e1cd01af75cd632c95f9f
buyanturb <buyantur1124@gmail.com>
2022-08-23 16:16:09 +0800

    header zassan

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,25 +9,46 @@ import bag from '../../assets/img/bag.svg'
 
 const Navbar = () =>
 {
+    const menus = [
+        {
+            name: "women"
+        },
+        {
+            name: "men"
+        },
+        {
+            name: "home"
+        },
+        {
+            name: "ACCESSORIES"
+        },
+        {
+            name: "organic"
+        }
+    ]
+
     return (
 		<div className="z-10 text-white">
             <TopHeader />
-            <div className="h-[88px] px-2 sm:px-11 flex justify-between items-center">
-                <div>
+            <div className="h-[88px] px-2 sm:px-11 flex items-center">
+                <div className="w-1/3">
                     <ul className="hidden md:flex uppercase cursor-pointer menus">
-                        <li className="mr-10"><a>Women</a></li>
-                        <li className="mr-10"><a>Men</a></li>
-                        <li className="mr-10"><a>Home</a></li>
-                        <li className="mr-10"><a>Accessories</a></li>
-                        <li className="mr-10"><a>Organic</a></li>
+                        {menus.map(
+                            (el, idx) =>
+                            {
+                                return(
+                                    <li className="mr-10" key={idx}><a>{el.name}</a></li>
+                                )
+                            }
+                        )}
                     </ul>
                 </div>
-                <div>
+                <div className="w-1/3 flex justify-center">
                     <img src={logo} alt="Logo" />
                 </div>
-                <div className="flex items-center">
-                    <input className="sm:block hidden placeholder:text-black block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="search" />
-                    <img className="px-3 cursor-pointer" src={like} alt="like" />
+                <div className="flex items-center w-1/3 justify-end">
+                    <input className="w-[260px] sm:block hidden placeholder:text-black block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="search" />
+                    <img className="px-5 cursor-pointer" src={like} alt="like" />
                     <img className="cursor-pointer" src={bag} alt="bag" />
                 </div>
             </div>
