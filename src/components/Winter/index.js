dc15dea5ccff12c7279b9ccc74b3233e6622adb9
buyanturb <buyantur1124@gmail.com>
2022-08-23 17:43:53 +0800

    collection zassan

--- src/components/Winter/index.js
+++ src/components/Winter/index.js
@@ -9,10 +9,12 @@ const Winter = () =>
             <div className="md:w-1/2">
                 <h3 className="main-title">winter spice 2020</h3>
                 <p>Infusion of summer pieces and cashmere silk blends.</p>
+                <button className="cursor-pointer rounded-lg text-lg md:px-10 md:py-3 px-8 py-2 bg-zinc-900 text-white cursor-pointer mt-6 uppercase font-medium m-auto block md:inline">shop now</button>
+
             </div>
-            <div className="md:w-1/2">
+            {/* <div className="md:w-1/2">
                 <img src={collection} />
-            </div>
+            </div> */}
         </div>
 	)
 }
